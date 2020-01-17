b941ad8c42cd2e392a87d0197bafb6bda424fa5c
Gladiel <gladiel.belen@adesa.com>
2020-01-17 13:32:28 -0500

    sample css change live update

---
+++ src/views/navigation/navigation.ts
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Component from "vue-class-component";
+
+@Component
+export default class Navigation extends Vue{
+
+  data(): any {
+      return {
+          attributes : [{title: 'menuTextColor',value:"#ffffff"},
+                        {title: 'menuTextHoverColor',value:'#ffffff'},
+                        {title: 'navBgColor',value:'#333333'},
+                        {title: 'navItemHoverColor',value:'#8a8585'},
+                        {title: 'menuFontType',value:'Calibri'},
+                        {title: 'mainMenuNavActiveStateFontColor',value:'#ffffff'},
+                        {title: 'submenuNavActiveStateFontColor',value:'#c0c0c0'},
+                        {title: 'submenuNavBackgroundBoxColor',value:'#c0c0c0'},
+                        {title: 'dropDownBackgroundColor',value:'#a8a8a8'},
+                        {title: 'dropDownTextHoverColor',value:'#000000'}]
+      };
+  }
+
+  mounted():void{
+  let root = document.documentElement;
+  root.style.setProperty('--font-color-blue','blue');
+    console.log(process.env.VUE_APP_SITE_CONFIGURATION_URL);
+  }
+
+  getStyleSheet():void{
+
+  }
+
+}
